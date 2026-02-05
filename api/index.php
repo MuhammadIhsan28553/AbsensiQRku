9ee5045e5c8ada917035e84a0fcd599b9a37024b
MuhammadIhsan28553 <ihsanmhmmaddahlan@gmail.com>
2026-02-05 17:34:33 +0700

    Configure storage path for Vercel deployment
    Updated index.php to configure storage path for Vercel.

--- api/index.php
+++ api/index.php
@@ -1,3 +1,19 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// 1. Load Autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+// 2. Load Aplikasi Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// === PERBAIKAN VERCEL (PENTING) ===
+// Mengalihkan folder storage ke /tmp karena Vercel Read-Only
+$app->useStoragePath('/tmp/storage');
+// ==================================
+
+// 3. Jalankan Aplikasi
+$app->handleRequest(Request::capture());
